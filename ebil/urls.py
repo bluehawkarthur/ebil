5bb47a8b787256e9d6eda55ea1abd25b39eb8a43
jose daniel <josedanielf9@gmail.com>
2016-02-16 17:58:31 -0400

    update url

--- ebil/urls.py
+++ ebil/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
+
 # import debug_toolbar
 
 urlpatterns = [
@@ -34,4 +36,4 @@ urlpatterns = [
     # url(r'^__debug__/', include(debug_toolbar.urls)),
     # url(r'^', include('apps.almacen.urls')),
     
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

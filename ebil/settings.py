65e0da9ad99e4a385418bfc6080e0ec813e3a05d
jose daniel <josedanielf9@gmail.com>
2016-03-31 16:20:09 -0400

    settings config

--- ebil/settings.py
+++ ebil/settings.py
@@ -155,11 +155,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
-]
-
 # ======== configure for user roles ===========
 ROLEPERMISSIONS_MODULE = 'apps.inicio.roles'
 
